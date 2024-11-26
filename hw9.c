13536dace2c74350408e94e1fcbfa63f181f64b9
smukmh123 <smukmh123@gmail.com>
2024-11-26 15:01:08 +0900

    Update hw9.c

--- hw9.c
+++ hw9.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
-#include <ctype.h>
+
+int convCase(int ch) {
+    const int diff = 'a' - 'A';
+    if (ch >= 'A' && ch <= 'Z') 
+        return ch + diff;        
+    else if (ch >= 'a' && ch <= 'z')  
+        return ch - diff;          
+    else
+        return -1;  
+}
 
 int main(void) {
-    char ch;
-
-    printf("문자열 입력: ");
-    
-    // 하나씩 입력받아서 처리
-    while ((ch = getchar()) != '\n') {  // Enter 키를 입력할 때까지 반복
-        if (ch >= 'A' && ch <= 'Z') {
-            putchar(ch + ('a' - 'A'));  // 대문자 -> 소문자
-        } else if (ch >= 'a' && ch <= 'z') {
-            putchar(ch - ('a' - 'A'));  // 소문자 -> 대문자
-        } else {
-            // 알파벳 외의 문자가 들어오면 오류 메시지 출력
-            printf("\n잘못된 입력입니다.\n");
-            return 1;
-        }
+    int ch;
+    printf("문자 입력: ");
+    ch = getchar();   
+    ch = convCase(ch); 
+    if (ch == -1) {
+        puts("범위를 벗어난 입력입니다.");
+        return -1;
     }
-
-    printf("\n");  // 줄 바꿈 출력
-
+    putchar(ch);  
     return 0;
 }
